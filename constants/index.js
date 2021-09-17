c0ba7e81319d907a354429e324be563e79cc0ff0
Noé <zalir0@live.fr>
2021-09-17 14:28:36 +0200

    local migration

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,6 @@
 const LKT_TOKEN = {
-  ['development']: '0x370bf023934c54F5A12c9d807037652Ea789454F', // Development
+  /* 0x370bf023934c54F5A12c9d807037652Ea789454F */
+  ['development']: '0x8437Dd55aE1cB53506Da0e4F7ad940Bb4EfB13aF', // Development
 
   ['ropsten']: '0xde8fa069707b6322ad45d001425b617f4f1930bd', // Ropsten
   ['eth_mainnet']: '0xd9b89eee86b15634c70cab51baf85615a4ab91a1', // Ethereum Mainnet
